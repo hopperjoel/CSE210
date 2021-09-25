8df49f5d8291be4f25cbdb5fea3cce1af9771cc6
Joel Hopper <hopper.joel.w@gmail.com>
2021-09-25 17:33:33 -0600

    Finished Dice-Solo

--- W02/dice_game/game/thrower.py
+++ W02/dice_game/game/thrower.py
@@ -14,29 +14,29 @@ class Thrower():
         self.die_4 = 0
         self.die_5 = 0
 
-        self.dice = [self.die_1, self.die_2, self.die_3, self.die_4, self.die_5]
+        self.dice = []
         self.num_throws = 0
+        self.points = 0
         
     #Variables for each die?
-    def throw_dice(self, throw_status):
+    def throw_dice(self):
         #Function for Throwing Dice by generating a random number between 1 and 6?
             #Returns array of random numbers?
         """
         This function receives the throw_status boolean and generates random integers between 1 and 6, then creates a list
         of all the obtained integers for each die variable, then returns the list of integers
         """
-        if throw_status:
-            self.die_1 = random.randint(1, 6)
-            self.die_2 = random.randint(1, 6)
-            self.die_3 = random.randint(1, 6)
-            self.die_4 = random.randint(1, 6)
-            self.die_5 = random.randint(1, 6)
-            self.dice = [self.die_1, self.die_2, self.die_3, self.die_4, self.die_5]
-            self.num_throws += 1
+        self.die_1 = random.randint(1, 6)
+        self.die_2 = random.randint(1, 6)
+        self.die_3 = random.randint(1, 6)
+        self.die_4 = random.randint(1, 6)
+        self.die_5 = random.randint(1, 6)
+        self.dice = [self.die_1, self.die_2, self.die_3, self.die_4, self.die_5]
+        self.num_throws += 1
 
         return self.dice, self.num_throws
     
-    def get_points(self, dice):
+    def get_points(self):
     #Function for getting the current points?
         #counts the number of 1s and 5s and sums the total?
         #returns the sum
@@ -46,7 +46,7 @@ class Thrower():
         by adding 1's and 5's. It returns this total amount of points.
         """
         self.points = 0
-        for i in dice:
+        for i in self.dice:
             if i == 1:
                 self.points += 100
 
@@ -55,7 +55,7 @@ class Thrower():
 
         return self.points
     
-    def can_throw(self, points, num_throws):
+    def can_throw(self):
     #Function to determine if a 1 or a 5 were thrown and if the player can continue
         #returns a boolean for keep_playing?
         """
@@ -63,12 +63,12 @@ class Thrower():
         total points score for that round (since rolling a 5 or a 1 would produce points). It
         returns a boolean value of throw_status
         """
-        if points > 0 or num_throws == 0:
-            self.throw_status = True
+        if self.points > 0 or self.num_throws == 0:
+            throw_status = True
         else:
-            self.throw_status = False
+            throw_status = False
 
-        return self.throw_status
+        return throw_status
 
 
     
